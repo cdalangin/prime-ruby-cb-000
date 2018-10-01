61342057ac4405d5f8206d62edbafbbb92a4cb6d
Cirill Florenz  Dalangin <florenzdalangin@gmail.com>
2018-10-01 21:04:13 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,15 @@
 # Add  code here!
 def prime?(n)
+  counter = 0
   if n == 0
     return false
   elsif (n % 1 == 0) && (n % n == 0) && (n > 1)
     return true
-  else
-    return false
+    numbers = (2..10000).to_a 
+    numbers.map do |num|
+      if n % num == 0
+        return false
+      end
+    end
   end
 end
